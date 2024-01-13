43befca6514b40384b67870afdf51c427bbe93a8
richirick <113588596+richirick@users.noreply.github.com>
2024-01-13 13:07:09 +0300

    Initial commit

---
+++ app/src/main/java/com/example/wordsapp/YourBroadcastReceiver.kt
@@ -0,0 +1,17 @@
+package com.example.wordsapp
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+
+//class YourBroadcastReceiver : BroadcastReceiver() {
+//
+//    override fun onReceive(context: Context, intent: Intent) {
+//        val receiver: BroadcastReceiver = YourBroadcastReceiver()
+//        val filter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+//        applicationContext.registerReceiver(receiver, filter)
+//
+//        TODO("YourBroadcastReceiver.onReceive() is not implemented")
+//    }
+//}
