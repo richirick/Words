417a9f00e5f3c6213ab0e28a755374c0855bfc6c
richirick <113588596+richirick@users.noreply.github.com>
2024-01-13 13:07:09 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/wordsapp/NavigationTests.kt
@@ -0,0 +1,47 @@
+package com.example.wordsapp
+
+import android.app.Application
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.navigation.Navigation
+import androidx.navigation.testing.TestNavHostController
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.runner.AndroidJUnit4
+import junit.framework.TestCase.assertEquals
+import org.junit.runner.RunWith
+
+
+@RunWith(AndroidJUnit4::class) //1. Указываю поля исполнителя тестов
+class NavigationTests {
+
+    //2. Создаю тестовую функцию:
+    fun navigate_tow_words_nav_component() {
+        //3. Создаю в методе тестовый экземпляр котроллера навигации.
+        val navController = TestNavHostController(
+            ApplicationProvider.getApplicationContext()
+        )
+        //4. Изоляция фрагмента для тестирования. Передаю тему приложения чтобы компоненты
+        //пользовательского интерфейса знали, какую тему использовать,
+        // иначе тест может завершиться сбоем.
+        val letterListScenario = launchFragmentInContainer<LetterListFragment>(themeResId =
+        R.style.Theme_Words)
+
+        //5. Явно объявляю желаемый граф навигации что бы котролер навигации использовал его для
+        //запуска фрагмента
+        letterListScenario.onFragment { fragment ->
+            navController.setGraph(R.navigation.nav_graph)
+
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+        //6. Запускаю событие которое запрашивает навигацию.
+        onView(withId(R.id.recycler_view))
+            .perform(RecyclerViewActions
+                .actionOnItemAtPosition<RecyclerView.ViewHolder>(2, click()))
+
+        assertEquals(navController.currentDestination?.id, R.id.wordListFragment)
+    }
+}
--- app/src/main/java/com/example/wordsapp/MainActivity.kt
+++ app/src/main/java/com/example/wordsapp/MainActivity.kt
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 package com.example.wordsapp
+//https://web.archive.org/web/20230607034554/https://developer.android.com/courses/pathways/android-basics-kotlin-unit-3-pathway-2
 
 //https://web.archive.org/web/20230927065325/https://developer.android.com/codelabs/basic-android-kotlin-training-fragments-navigation-component?continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-kotlin-unit-3-pathway-2%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-training-fragments-navigation-component#8
 import android.content.Intent
